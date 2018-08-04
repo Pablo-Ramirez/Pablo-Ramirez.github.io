51c824f9e1619f41ae9540f11c101908208b45fe
Pablo-Ramirez <32146545+Pablo-Ramirez@users.noreply.github.com>
2018-08-04 16:22:18 -0700

    Fix mobile version of Development page
    Fixed the website images to fit the page correctly and not overflow off the page. Also a minor change to the pulse action going on the WELCOME TO MY PORTFOLIO in the mobile version

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 $(document).ready(function(){
   $('#navbar').load('header.html');
 });
- window.onscroll = function() {
-   myFunction()
- };
- var header = document.getElementById('navbar');
- var sticky = header.offsetTop;
- function myFunction(){
-   if(window.pageYOffset > sticky){
-     header.classList.add('sticky');
-   } else{
-     header.classList.remove('sticky');
-   }
- }
